bb6c4128a4c4b5b130851a30a63668304fd9ae5f
gituder <ksujay12@gmail.com>
2019-02-06 00:48:46 +0530

    question-13

--- C/question-13/question-13/Source.c
+++ C/question-13/question-13/Source.c
@@ -1,26 +1,54 @@
 #include<stdio.h>
-#include<string.h>
+void fun(int, int, int);
 int main()
 {
-	int p, b, i;
-
-	char a[10];
-	scanf_s("%s", a);
+	int n, p, b;
+	scanf_s("%d", &n);
 	scanf_s("%d", &p);
 	scanf_s("%d", &b);
-	for (i = p - 1; i < p + b + 1; i++)
+	fun(n, p, b);
+	getch();
+	return 0;
+
+}
+
+void fun(int n, int p, int b)
+{
+	int i = 0, e;
+	int s[10], s1[10], k, h, j;
+	while (n != 0)
 	{
-		if ((strcmp(a[i], '0') == 1)
+
+		s[i] = n % 2;
+		n = n / 2;
+		i++;
+	}
+
+
+	for (h = 0, k = i - 1;h < i, k >= 0;h++, k--)
+	{
+
+		s1[h] = s[k];
+
+	}
+
+
+	for (j = p;j < p + b;j++)
+	{
+		if (s1[j] == 0)
 		{
-			a[i] = '1';
+			s1[j] = 1;
 		}
 		else
 		{
-			a[i] = '0';
+			s1[j] = 0;
 		}
 	}
-	printf("%s", a);
-	getch();
-	return 0;
-	
+
+
+	for (e = 0;e < i;e++)
+	{
+		printf("%d", s1[e]);
+	}
+
 }
