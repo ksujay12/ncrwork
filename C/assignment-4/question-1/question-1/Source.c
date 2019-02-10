767b87a8912f73aff5f7802b3e692ab94eb20b4c
gituder <ksujay12@gmail.com>
2019-02-10 16:26:55 +0530

    q-1

---
+++ C/assignment-4/question-1/question-1/Source.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	float table[2][3] = { {1.1,1.2,1.3},{2.1,2.2,2.3} };
+	
+	printf("%lu\n", table);
+	printf("%lu\n", *table);
+	printf("%lu\n", (table + 1));
+	
+	printf("%lu\n", *(table + 1));
+
+	printf("%lu\n", (*(table + 1) + 1));
+	
+	printf("%f\n", *(*(table + 1) + 1));
+	printf("%lu\n", (*(table)+1));
+
+	printf("%f\n", *(*(table)+1));
+	
+	printf("%f\n", *(*(table)+1));
+
+	printf("%f\n", *(*(table + 1)));
+
+	printf("%f\n", *(*(table)+1) + 1);
+	
+	getch();
+	return 0;
+}
