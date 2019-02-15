8e176ede656212d0ede119b2e708a5eebb0e35ca
gituder <ksujay12@gmail.com>
2019-02-15 14:51:41 +0530

    q-3

--- c++/assignment-2/question-3/question-3/Source.cpp
+++ c++/assignment-2/question-3/question-3/Source.cpp
@@ -1,15 +1,35 @@
+#include<iostream>
+using namespace std;
 class employee
 {
 	int	empno;
 	int salary;
 	char name[10];
-
-	employee()
+public:
+ void setdata()
 	{
+	 cin >> empno;
+	 cin >> name;
+	 cin >> salary;
 
 	}
-	employee(char s[10],int e1,int sal)
-	{
-		cin << s;
-	}
+ friend ostream& operator<<(ostream &cout, employee x);
 };
+
+ostream& operator<<(ostream &cout, employee x)
+{
+	cout << x.empno << endl;
+	cout << x.name << endl;
+	cout << x.salary << endl;
+	return cout;
+}
+int main()
+{
+	employee r[2];
+	r[0].setdata();
+	r[1].setdata();
+	cout << r[0];
+	cout << r[1];
+	system("pause");
+	return 0;
+}
