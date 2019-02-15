b7a1bbee167d3e8fea257576c60c1da0fee53244
gituder <ksujay12@gmail.com>
2019-02-15 16:26:01 +0530

    q-4

--- c++/assignment-2/question-3/question-3/Source.cpp
+++ c++/assignment-2/question-3/question-3/Source.cpp
@@ -25,11 +25,17 @@ ostream& operator<<(ostream &cout, employee x)
 }
 int main()
 {
-	employee r[2];
+	employee r[5];
 	r[0].setdata();
 	r[1].setdata();
+	r[2].setdata();
+	r[3].setdata();
+	r[4].setdata();
 	cout << r[0];
 	cout << r[1];
+	cout << r[2];
+	cout << r[3];
+	cout << r[4];
 	system("pause");
 	return 0;
 }
